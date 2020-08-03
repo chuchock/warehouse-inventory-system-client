08f1b34dadb12aa4035b74303b74976b9e4a9c8e
Jesús Montero <jesusmontero.developer@gmail.com>
2020-08-03 13:51:07 -0500

    Added state to Products

--- src/pages/ProductNew.js
+++ src/pages/ProductNew.js
@@ -1,48 +1,118 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const ProductNew = () => (
-	<Fragment>
-		<Link to="/products" className="btn btn-link">Back</Link>
-		<br /><br />
+const ProductNew = () => {
 
-		<form>
-			<div className="form-group">
-				<label htmlFor="txt-name">Name</label>
-				<input type="text" className="form-control" id="txt-name" placeholder="" />
-			</div>
+	const [product, updateProduct] = useState({
+		name: '',
+		description: '',
+		quantity: 0,
+		buyPrice: 0.0,
+		salePrice: 0.0,
+		idCategory: -1
+	});
 
-			<div className="form-group">
-				<label htmlFor="">Description</label>
-				<input type="text" className="form-control" id="txt-description" placeholder="" />
-			</div>
+	const updateState = e => {
+		updateProduct({
+			...product,
+			[e.target.name]: e.target.value
+		})
+	}
 
-			<div className="form-row">
-				<div className="form-group col-md-4">
-					<label htmlFor="">Quantity</label>
-					<input type="number" className="form-control" id="txt-quantity" />
+	const { name, description, quantity, buyPrice, salePrice, idCategory } = product;
+
+	const submitProduct = e => {
+		e.preventDefault();
+
+		// restart form
+		updateAppointment({
+			name: '',
+			description: '',
+			quantity: 0,
+			buyPrice: 0.0,
+			salePrice: 0.0,
+			idCategory: -1
+		})
+	}
+
+	return (
+		<Fragment>
+			<Link to="/products" className="btn btn-link">Back</Link>
+			<br /><br />
+
+			<form
+				onSubmit={submitProduct}
+			>
+				<div className="form-group">
+					<label>Name</label>
+					<input
+						type="text"
+						className="form-control"
+						name="name"
+						placeholder=""
+						onChange={updateState}
+						value={name}
+					/>
 				</div>
-				<div className="form-group col-md-4">
-					<label htmlFor="">Buy price</label>
-					<input type="text" className="form-control" id="txt-buyprice" />
+
+				<div className="form-group">
+					<label>Description</label>
+					<input
+						type="text"
+						className="form-control"
+						name="description"
+						placeholder=""
+						onChange={updateState}
+						value={description}
+					/>
 				</div>
-				<div className="form-group col-md-4">
-					<label htmlFor="">Sale price</label>
-					<input type="text" className="form-control" id="txt-saleprice" />
+
+				<div className="form-row">
+					<div className="form-group col-md-4">
+						<label>Quantity</label>
+						<input
+							type="number"
+							className="form-control"
+							name="quantity"
+							onChange={updateState}
+							value={quantity} />
+					</div>
+					<div className="form-group col-md-4">
+						<label>Buy price</label>
+						<input
+							type="text"
+							className="form-control"
+							name="buyPrice"
+							onChange={updateState}
+							value={buyPrice}
+						/>
+					</div>
+					<div className="form-group col-md-4">
+						<label>Sale price</label>
+						<input
+							type="text"
+							className="form-control"
+							name="salePrice"
+							onChange={updateState}
+							value={salePrice}
+						/>
+					</div>
 				</div>
-			</div>
 
-			<div className="form-group">
-				<label htmlFor="sel-category">Category</label>
-				<select id="sel-category" className="form-control">
-					<option value="" selected>Choose...</option>
-				</select>
-			</div>
+				<div className="form-group">
+					<label>Category</label>
+					<select
+						name="idCategory"
+						className="form-control">
+						<option>Choose...</option>
+					</select>
+				</div>
 
-			<button className="btn btn-primary">Save</button>
-		</form>
+				<button className="btn btn-primary">Save</button>
+			</form>
 
-	</Fragment>
-);
+		</Fragment>
+	);
+}
 
 export default ProductNew;
