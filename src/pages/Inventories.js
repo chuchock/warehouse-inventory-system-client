5ad1337aeed8bbf2adf194d2617c0046af023bf9
Jesús Montero <jesusmontero.developer@gmail.com>
2020-08-11 18:22:48 -0500

    Get inventories functionality

--- src/pages/Inventories.js
+++ src/pages/Inventories.js
@@ -11,8 +11,18 @@ const Inventories = () => {
 	const [warehouses, setWarehouses] = useState([]);
 
 	useEffect(() => {
-		InventoryService.getInventoryByWarehouse().then(
+		WarehouseService.getWarehouses().then(
 			(response) => {
+				setWarehouses(response.data);
+			},
+			(error) => {
+				console.log("error: " + error);
+			}
+		);
+
+		InventoryService.getInventoriesByWarehouse(1).then(
+			(response) => {
+				console.log(response);
 				setInventories(response.data);
 			},
 			(error) => {
@@ -20,6 +30,17 @@ const Inventories = () => {
 			}
 		);
 	}, []);
+
+	const handleChange = e => {
+		InventoryService.getInventoriesByWarehouse(e.target.value).then(
+			(response) => {
+				setInventories(response.data);
+			},
+			(error) => {
+				console.log("error: " + error);
+			}
+		);
+	}
 
 	return (
 		<div>
@@ -32,9 +53,8 @@ const Inventories = () => {
 				<div className="form-group">
 					<label>Warehouse</label>
 					<select
-						// name="warehouseId"
 						className="form-control"
-						// value={warehouseId}
+						onChange={handleChange}
 					>
 						{warehouses.map(warehouse => {
 							return (
@@ -47,21 +67,24 @@ const Inventories = () => {
 					</select>
 				</div>
 
-				<button className="btn btn-primary">Search</button>
+				{/* <button className="btn btn-primary">Search</button> */}
 			</form>
 
 			<br></br>
 			<table className="table">
 				<thead>
 					<tr>
-						<th scope="col">Name</th>
+						<th scope="col">Product</th>
+						<th scope="col">Quantity</th>
+						<th></th>
 					</tr>
 				</thead>
 				<tbody>
 					{inventories.map(inventory => {
 						return (
 							<tr key={inventory.inventoryId}>
-								<td>{inventory.name}</td>
+								<td>{inventory.product.name}</td>
+								<td>{inventory.quantity}</td>
 								<td className="table-actions">
 									<button type="button" className="btn btn-primary"><i className="fas fa-eye"></i></button>
 									<button type="button" className="btn btn-success"><i className="fas fa-edit"></i></button>
