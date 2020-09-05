82021d62b0d17e9b1026f443ae61fee3d1487881
Jesús Montero <jesusmontero.developer@gmail.com>
2020-09-05 15:41:55 -0500

    show/hide warehouses table

--- src/domain/Warehouses.js
+++ src/domain/Warehouses.js
@@ -34,34 +34,40 @@ const Warehouses = () => {
 			<Link to="/warehouses/new" className="btn btn-primary">Add new</Link>
 			<br /><br />
 
-			<table className="table">
-				<thead>
-					<tr>
-						<th scope="col">Name</th>
-						<th scope="col">Address</th>
-						<th scope="col"></th>
-					</tr>
-				</thead>
-				<tbody>
-					{warehouses.map(warehouse => {
-						return (
-							<tr key={warehouse.warehouseId}>
-								<td>{warehouse.name}</td>
-								<td>{warehouse.address}</td>
-								<td className="table-actions">
-									<button type="button" className="btn btn-primary"><i className="fas fa-eye"></i></button>
-									<button type="button" className="btn btn-success"><i className="fas fa-edit"></i></button>
-									<button
-										type="button"
-										className="btn btn-danger"
-										onClick={() => deleteWarehouse(warehouse.warehouseId)}
-									><i className="fas fa-trash-alt"></i></button>
-								</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
+			{warehouses.length === 0 ? (
+				<div className="alert alert-info" role="alert">
+					There are no registered warehouses.
+				</div>
+			) :
+				<table className="table">
+					<thead>
+						<tr>
+							<th scope="col">Name</th>
+							<th scope="col">Address</th>
+							<th scope="col"></th>
+						</tr>
+					</thead>
+					<tbody>
+						{warehouses.map(warehouse => {
+							return (
+								<tr key={warehouse.warehouseId}>
+									<td>{warehouse.name}</td>
+									<td>{warehouse.address}</td>
+									<td className="table-actions">
+										<button type="button" className="btn btn-primary"><i className="fas fa-eye"></i></button>
+										<button type="button" className="btn btn-success"><i className="fas fa-edit"></i></button>
+										<button
+											type="button"
+											className="btn btn-danger"
+											onClick={() => deleteWarehouse(warehouse.warehouseId)}
+										><i className="fas fa-trash-alt"></i></button>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			}
 		</div>
 	);
 }
